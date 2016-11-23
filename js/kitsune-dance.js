c019fad20ad8c993d09666b30f72b8b7170cac33
latiosu <latiosworks@gmail.com>
2016-11-23 12:40:20 +1100

    Refactor and center game display

---
+++ js/kitsune-dance.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(640, 360, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+
+function preload() {
+
+}
+
+function create() {
+  
+}
+
+function update() {
+
+}
